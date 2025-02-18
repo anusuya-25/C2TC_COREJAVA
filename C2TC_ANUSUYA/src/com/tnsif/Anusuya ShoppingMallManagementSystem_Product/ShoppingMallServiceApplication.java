81abf302e36073db4f9ab6605cbef7a48dee3be7
ANUSUYA A <anusuyaalagirisamy@gmail.com>
2025-02-18 12:49:34 +0530

    Add files via upload

---
+++ C2TC_ANUSUYA/src/com/tnsif/Anusuya ShoppingMallManagementSystem_Product/ShoppingMallServiceApplication.java
@@ -0,0 +1,12 @@
+// Product Service Application Class
+package com.tns.shoppingmallservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallServiceApplication.class, args);
+    }
+}
